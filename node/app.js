c4bf58fb5ba51ac598b88bf588a74cc575fb5f37
Ravi Chandra <krchandra88@gmail.com>
2017-05-09 20:58:16 +0000

    initial commit

--- node/app.js
+++ node/app.js
@@ -1,2 +1,21 @@
-var aloha ='Aloha variables';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'))
+app.get('/', function(req, res){
+    res.send('ravi hi');
+});
+
+app.get('/routing', function(req, res){
+    res.send('ravi hi Routing');
+});
+
+
+app.listen(port, function(err){
+    console.log('The server is running on port :' +port);
+    
+});
